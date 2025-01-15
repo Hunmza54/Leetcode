a4480a02769468ac2afea942a58f25b43dd8b363
Hunmza54 <saeedh20@mcmaster.ca>
2025-01-15 08:41:54 -0500

    added files

---
+++ longest_conseq.js
@@ -0,0 +1,20 @@
+var longest_conseq = function (arr) {
+
+    const mySet = new Set(arr);
+    var count = 0;
+    for (let i = 0;i<arr.length;i++){
+        if (mySet.has(arr[i]-1)){
+           count = count;
+        }
+        else{
+        
+        }
+
+    }
+
+
+
+};
+
+
+nums = [2,20,4,10,3,4,5];
